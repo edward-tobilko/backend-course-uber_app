4bbd2b24827888ceed1ae166335192c38513d34f
eduard.tobilko <1992eduard777@gmail.com>
2025-09-02 19:41:11 -0400

    CRUD and middleware

--- server/server.ts
+++ server/server.ts
@@ -1,28 +1,198 @@
-import express = require("express");
+import express, { Request, Response } from "express";
+import { log, error, warn } from "node:console";
 
 const app = express();
 const port = 3007;
 
-app.get("/", (req, res) => {
-  let a = 5;
-  let b = 10;
+// ? VARIABLES
+const HTTP_CODES = {
+  OK_200: 200,
+  CREATED_201: 201,
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404,
+};
 
-  if (b > 7) {
-    res.send("Orders");
-  } else {
-    res.send("Items");
+// ? HELPER FUNCTIONS
+const normalizeName = (input: string) => {
+  return typeof input === "string" ? input.trim() : "";
+};
+
+// ? MIDDLEWARE - створюємо проміжуточний шар для body, для метода post
+const jsonBodyMiddleware = express.json();
+
+app.use(jsonBodyMiddleware);
+
+// ? TYPES
+type CourseName = "front-end" | "back-end" | "devops" | "full-stack";
+
+interface Course {
+  courseId: number;
+  name: CourseName;
+}
+
+interface DataBase {
+  courses: Course[];
+}
+
+// ? DATA
+const dataBase: DataBase = {
+  courses: [
+    { courseId: 1, name: "front-end" },
+    { courseId: 2, name: "back-end" },
+    { courseId: 3, name: "devops" },
+    { courseId: 4, name: "full-stack" },
+  ],
+};
+
+// ? method GET
+app.get(
+  "/courses",
+  (req: Request<{}, {}, {}, { name: string }>, res: Response) => {
+    let filteredCourses = dataBase.courses;
+
+    if (req.query.name) {
+      filteredCourses = filteredCourses.filter((filteredCourse) =>
+        filteredCourse.name.includes(req.query.name)
+      );
+    }
+
+    res.status(HTTP_CODES.OK_200).send(filteredCourses);
   }
+);
+
+app.get(`/courses/:id`, (req: Request<{ id: number }>, res: Response) => {
+  const foundCourse = dataBase.courses.find(
+    (course) => course.courseId === +req.params.id
+  );
+
+  if (!foundCourse) {
+    return res.sendStatus(HTTP_CODES.NOT_FOUND_404);
+  }
+
+  res.status(HTTP_CODES.OK_200).json(foundCourse);
 });
 
-app.get("/items", (req, res) => {
-  setTimeout(() => {
-    res.send("Items");
-  }, 2000);
+// ? method POST
+app.post(`/courses`, (req: Request<{ name: string }>, res: Response) => {
+  const { name } = req.body ?? {};
+
+  if (!name) return res.sendStatus(HTTP_CODES.BAD_REQUEST_400);
+
+  if (typeof name !== "string" || name.trim().length === 0)
+    return res
+      .status(HTTP_CODES.BAD_REQUEST_400)
+      .json({ message: "Invalid name. Name must be of type string!" });
+
+  const newCourse: Course = {
+    courseId: +new Date(),
+    name: req.body.name,
+  };
+
+  dataBase.courses.push(newCourse);
+
+  log(newCourse);
+
+  res.status(HTTP_CODES.CREATED_201).json(newCourse);
 });
 
-app.post("/items", (req, res) => {
-  res.send("new item");
+// ? method DELETE
+app.delete("/courses/:id", (req: Request<{ id: number }>, res: Response) => {
+  const id = +req.params.id;
+
+  // знаходимо індекс курсу, якого ми хочемо видалити
+  const index = dataBase.courses.findIndex((index) => index.courseId === id);
+
+  // перевіряємо, якщо курс не знайдено
+  if (index === -1) {
+    return res
+      .status(HTTP_CODES.NOT_FOUND_404)
+      .json({ message: `Course with id=${id} not found` });
+  }
+
+  // видаляємо один елемент
+  const [deletedCourse] = dataBase.courses.splice(index, 1);
+
+  // повертаємо підтвердження
+  res
+    .status(HTTP_CODES.OK_200) // ok
+    .json({
+      message: `Course id=${id} deleted successfully`,
+      deleted: deletedCourse,
+    });
 });
+
+// ? method UPDATE
+app.put(
+  "/courses/:id",
+  (req: Request<{ id: number }, { name: string }>, res: Response) => {
+    const id = +req.params.id;
+
+    // знаходимо індекс курсу, якого ми хочемо обновити
+    const index = dataBase.courses.findIndex((index) => index.courseId === id);
+
+    log(index);
+
+    // перевіряємо, якщо курс не знайдено
+    if (index === -1) {
+      return res
+        .status(HTTP_CODES.NOT_FOUND_404)
+        .json({ message: `Course with id=${id} not found` });
+    }
+
+    // отримуємо нову строку
+    const raw = normalizeName(req.body.name);
+
+    log(raw);
+
+    // перевіряємо на валідність нову строку
+    if (!raw)
+      return res
+        .status(HTTP_CODES.BAD_REQUEST_400)
+        .json({ message: "Name cannot be empty or spaces only" });
+
+    dataBase.courses[index].name = raw as CourseName;
+
+    res.status(HTTP_CODES.OK_200).json({
+      message: `Course id=${id} updated successfully`,
+      updatedCourse: dataBase.courses[index],
+    });
+  }
+);
+
+app.patch(
+  "/courses/:id",
+  (req: Request<{ id: number }, { name: string }>, res: Response) => {
+    const id = +req.params.id;
+
+    // знаходимо обʼєкт (курс), який ми будемо оновляти
+    const course = dataBase.courses.find((course) => course.courseId === id);
+
+    log(course);
+
+    // перевіряємо, якщо обʼєкт не знайдено
+    if (!course)
+      return res
+        .status(HTTP_CODES.NOT_FOUND_404)
+        .json({ message: `Course with id=${id} not found` });
+
+    // отримуємо нову строку
+    const raw = normalizeName(req.body.name);
+
+    // перевіряємо на валідність нову строку
+    if (!raw)
+      return res
+        .status(HTTP_CODES.BAD_REQUEST_400)
+        .json({ message: "Name cannot be empty or spaces only" });
+
+    // оновлюємо лише те, що передали
+    course.name = raw as CourseName;
+
+    res.status(HTTP_CODES.OK_200).json({
+      message: `Course id=${id} updated successfully`,
+      updatedCourse: course,
+    });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
