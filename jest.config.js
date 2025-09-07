add8f7c47df50b6283a26e03abad51f6202627e8
eduard.tobilko <1992eduard777@gmail.com>
2025-09-07 17:31:06 -0400

    added jest-config file

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "tsx", "js"],
+
+  // шукаємо і юніт, і e2e:
+  testMatch: [
+    "**/?(*.)+(spec|test).ts",
+    "<rootDir>/src/__tests__/e2e/**/*.(spec|test).ts",
+  ],
+
+  clearMocks: true,
+  coverageDirectory: "coverage",
+
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
+
+module.exports = config;
+
+// ? createDefaultPreset() — створює дефолтні налаштування для ts-jest.
+// ? transform — описує, як Jest має трансформувати .ts файли перед виконанням тестів, без цього Jest не зрозуміє TypeScript і впаде з помилкою.
