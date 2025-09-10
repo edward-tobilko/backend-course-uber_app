b01d1b448e7a1784007fa8292c9ca48d4d730521
eduard.tobilko <1992eduard777@gmail.com>
2025-09-10 13:37:16 -0400

    driver validation

---
+++ src/config/driver.schema.ts
@@ -0,0 +1,15 @@
+import z from 'zod';
+
+import { VehicleFeature } from '@/types/drivers.types';
+
+export const driverSchema = z.object({
+  name: z.string().min(1),
+  phoneNumber: z.string(),
+  email: z.string().email(),
+  vehicleMake: z.string(),
+  vehicleModel: z.string(),
+  vehicleYear: z.number().min(1900).max(new Date().getFullYear()),
+  vehicleLicensePlate: z.string(),
+  vehicleDescription: z.string().nullable().optional(),
+  vehicleFeatures: z.nativeEnum(VehicleFeature).array(),
+});
