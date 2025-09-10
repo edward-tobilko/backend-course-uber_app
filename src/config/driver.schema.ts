aba9fad0ad215dc263cbc4f772ed94dac0701c62
eduard.tobilko <1992eduard777@gmail.com>
2025-09-10 14:52:28 -0400

    initial commit

--- src/config/driver.schema.ts
+++ src/config/driver.schema.ts
@@ -1,8 +1,9 @@
-import z from 'zod';
+import { z } from 'zod';
 
 import { VehicleFeature } from '@/types/drivers.types';
 
 export const driverSchema = z.object({
+  id: z.number().int().positive(),
   name: z.string().min(1),
   phoneNumber: z.string(),
   email: z.string().email(),
@@ -12,4 +13,5 @@ export const driverSchema = z.object({
   vehicleLicensePlate: z.string(),
   vehicleDescription: z.string().nullable().optional(),
   vehicleFeatures: z.nativeEnum(VehicleFeature).array(),
+  createdAt: z.date(),
 });
