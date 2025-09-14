e1d3cc70d2b2d66f1d00cfab846d491831c6d6f5
eduard.tobilko <1992eduard777@gmail.com>
2025-09-14 18:05:16 -0400

    validation

--- src/__tests__/e2e/server.e2e.test.ts
+++ src/__tests__/e2e/server.e2e.test.ts
@@ -2,13 +2,74 @@ import express from 'express';
 import request from 'supertest';
 
 import { setupApp } from '../../app';
+import { DriverInputDto } from '../../drivers/dto/driver.input-dto';
+import { HTTP_STATUS_CODES } from '../../core/types/http-statuses';
 
 describe('E2E: Drivers API', () => {
   const app = express();
   setupApp(app);
 
-  it("GET '/' and '/drivers' -> status code 200 and array of drivers", async () => {
-    const res = await request(app).get('/');
-    expect(res.status).toBe(200);
+  const testDriverData: DriverInputDto = {
+    name: 'Valentin',
+    phoneNumber: '123-456-7890',
+    email: 'valentin@example.com',
+    vehicleMake: 'BMW',
+    vehicleModel: 'X5',
+    vehicleYear: 2021,
+    vehicleLicensePlate: 'ABC-123',
+    vehicleDescription: null,
+    vehicleFeatures: [],
+  };
+
+  beforeAll(async () => {
+    await request(app)
+      .delete('/testing/all-data')
+      .expect(HTTP_STATUS_CODES.NO_CONTENT_204);
+  });
+
+  it('POST: /drivers -> should create driver', async () => {
+    await request(app)
+      .post('/drivers')
+      .send({
+        ...testDriverData,
+        name: 'Valentin',
+        phoneNumber: '123-456-7890',
+        email: 'valentin@example.com',
+      })
+      .expect(HTTP_STATUS_CODES.CREATED_201);
+  });
+
+  it("GET: '/' and '/drivers' -> should return drivers list", async () => {
+    await request(app)
+      .post('/drivers')
+      .send({
+        ...testDriverData,
+        name: 'Another driver 1',
+        vehicleLicensePlate: 'XYZ-111',
+      })
+      .expect(HTTP_STATUS_CODES.CREATED_201);
+
+    await request(app)
+      .post('/drivers')
+      .send({
+        ...testDriverData,
+        name: 'Another driver 2',
+        vehicleLicensePlate: 'XYZ-222',
+      })
+      .expect(HTTP_STATUS_CODES.CREATED_201);
+
+    const driverListResponseRoot = await request(app)
+      .get('/')
+      .expect(HTTP_STATUS_CODES.OK_200);
+
+    expect(driverListResponseRoot.body).toBeInstanceOf(Array);
+    expect(driverListResponseRoot.body.length).toBeGreaterThanOrEqual(2);
+
+    const driverListResponseDrivers = await request(app)
+      .get('/drivers')
+      .expect(HTTP_STATUS_CODES.OK_200);
+
+    expect(driverListResponseDrivers.body).toBeInstanceOf(Array);
+    expect(driverListResponseDrivers.body.length).toBeGreaterThanOrEqual(2);
   });
 });
