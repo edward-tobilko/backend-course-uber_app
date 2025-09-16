2fe2884cf02735435c80ac5b28ee33adfad118f9
eduard.tobilko <1992eduard777@gmail.com>
2025-09-16 13:39:40 -0400

    tests

--- src/__tests__/e2e/server.e2e.test.ts
+++ src/__tests__/e2e/server.e2e.test.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import request from 'supertest';
+import { log } from 'node:console';
 
 import { setupApp } from '../../app';
 import { DriverInputDto } from '../../drivers/dto/driver.input-dto';
 import { HTTP_STATUS_CODES } from '../../core/types/http-statuses';
+import { Driver } from '../../drivers/types/driver.types';
 
 describe('E2E: Drivers API', () => {
   const app = express();
-  setupApp(app);
+  setupApp(app); // подключает все middleware, маршруты и необходимые настройки
 
   const testDriverData: DriverInputDto = {
     name: 'Valentin',
@@ -28,14 +30,13 @@ describe('E2E: Drivers API', () => {
   });
 
   it('POST: /drivers -> should create driver', async () => {
+    const newDriver: DriverInputDto = {
+      ...testDriverData,
+    };
+
     await request(app)
       .post('/drivers')
-      .send({
-        ...testDriverData,
-        name: 'Valentin',
-        phoneNumber: '123-456-7890',
-        email: 'valentin@example.com',
-      })
+      .send(newDriver)
       .expect(HTTP_STATUS_CODES.CREATED_201);
   });
 
@@ -44,7 +45,7 @@ describe('E2E: Drivers API', () => {
       .post('/drivers')
       .send({
         ...testDriverData,
-        name: 'Another driver 1',
+        name: 'Another driver1',
         vehicleLicensePlate: 'XYZ-111',
       })
       .expect(HTTP_STATUS_CODES.CREATED_201);
@@ -53,7 +54,7 @@ describe('E2E: Drivers API', () => {
       .post('/drivers')
       .send({
         ...testDriverData,
-        name: 'Another driver 2',
+        name: 'Another driver2',
         vehicleLicensePlate: 'XYZ-222',
       })
       .expect(HTTP_STATUS_CODES.CREATED_201);
@@ -71,5 +72,27 @@ describe('E2E: Drivers API', () => {
 
     expect(driverListResponseDrivers.body).toBeInstanceOf(Array);
     expect(driverListResponseDrivers.body.length).toBeGreaterThanOrEqual(2);
+  });
+
+  it('GET: /drivers/:driverId should return driver by id', async () => {
+    const createResponse = (await request(app)
+      .post('/drivers')
+      .send({ ...testDriverData, name: 'Another driver' })
+      .expect(HTTP_STATUS_CODES.CREATED_201)) as request.Response & {
+      body: Driver;
+    };
+
+    const getResponse = await request(app)
+      .get(`/drivers/${createResponse.body.id}`)
+      .expect(HTTP_STATUS_CODES.OK_200);
+
+    expect(getResponse.body).toEqual({
+      ...createResponse.body,
+      id: expect.any(Number),
+      createdAt: expect.any(String),
+    });
+
+    log('createResponse ->', createResponse.status, createResponse.body);
+    log('getResponse ->', getResponse.status, getResponse.body);
   });
 });
