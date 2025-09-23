55316ab83a93a3e03bcca52dd1f5b36c6cf8f7f9
eduard.tobilko <1992eduard777@gmail.com>
2025-09-23 19:17:39 -0400

    lesson_2

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,18 @@ import express, { Express, Request, Response } from 'express';
 
 import { driversRouter } from './drivers/routers/drivers.router';
 import { testingRouter } from './drivers/routers/testing.router';
+import { DRIVERS_PATH, ROOT_PATH, TESTING_PATH } from './core/paths/paths';
 
 export const setupApp = (app: Express) => {
   app.use(express.json()); // * middleware для парсинга JSON в теле (body) запроса
 
-  app.get('/', (_req: Request, res: Response) => {
+  app.get(ROOT_PATH, (_req: Request, res: Response) => {
     res.status(200).send('hello world!!!');
   });
 
   // * Подключаем роутеры
-  app.use('/drivers', driversRouter);
-  app.use('/testing', testingRouter);
+  app.use(DRIVERS_PATH, driversRouter);
+  app.use(TESTING_PATH, testingRouter);
 
   return app;
 };
