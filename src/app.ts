57c2d93686cc374483821f78e506a5e0168c3ac0
eduard.tobilko <1992eduard777@gmail.com>
2025-09-23 16:04:20 -0400

    lesson_1-added root path

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 
 import { driversRouter } from './drivers/routers/drivers.router';
 import { testingRouter } from './drivers/routers/testing.router';
 
 export const setupApp = (app: Express) => {
   app.use(express.json()); // * middleware для парсинга JSON в теле (body) запроса
+
+  app.get('/', (_req: Request, res: Response) => {
+    res.status(200).send('hello world!!!');
+  });
 
   // * Подключаем роутеры
   app.use('/drivers', driversRouter);
