cf3cf8c7c96c21cfede6dcebb7c9adf2227bc3ed
eduard.tobilko <1992eduard777@gmail.com>
2025-09-24 14:37:37 -0400

    lesson_2-basic authorization

---
+++ src/auth/middlewares/admin-guard.middleware.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { HTTP_STATUS_CODES } from '../../core/utils/http-statuses';
+
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'admin';
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'qwerty';
+
+export const adminGuardMiddlewareAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const auth = req.headers['authorization'] as string; // 'Basic xxxx' - Получаем заголовок Authorization из запроса.
+
+  if (!auth) {
+    return res.sendStatus(HTTP_STATUS_CODES.UNAUTHORIZED_401);
+  }
+
+  const [authType, token] = auth.split(' '); // admin:qwerty - разбиваем строку по пробелу, получая тип авторизации (Basic) и сам токен.
+
+  if (authType !== 'Basic')
+    return res.sendStatus(HTTP_STATUS_CODES.UNAUTHORIZED_401);
+
+  const credentials = Buffer.from(token, 'base64').toString('utf-8'); // dbcadkcnasdk - Расшифровываем токен из base64 в обычную строку.
+
+  const [username, password] = credentials.split(':'); // разделяем её на логин и пароль.
+
+  if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
+    return res.sendStatus(HTTP_STATUS_CODES.UNAUTHORIZED_401);
+  }
+
+  next(); // Успешная авторизация, продолжаем.
+};
+
+// ? Explanation:
+// * Токен — это строка, закодированная в формате base64, которая представляет собой username:password.
+// * process - это глобальный объект в Node.js, который предоставляет информацию о текущем процессе Node.js
+// * env — это объект, который хранит все переменные окружения текущего процесса. Переменные окружения — это значения, которые могут быть установлены на уровне операционной системы или приложения и которые могут использоваться для настройки поведения программного обеспечения (например, пароли, ключи API, пути к файлам и т. д.).
--- src/drivers/routers/drivers.router.ts
+++ src/drivers/routers/drivers.router.ts
@@ -9,8 +9,11 @@ import { updateDriverPatchHandler } from './handlers/update-driver-patch.handler
 import { idParamValidation } from '../../core/middlewares/validation/params-id-validation.middleware';
 import { driverInputBodyDtoValidation } from '../validation/driver-input-dto-validation.middlewares';
 import { inputValidationResultMiddleware } from '../../core/middlewares/validation/input-validation-result.middleware';
+import { adminGuardMiddlewareAuth } from '../../auth/middlewares/admin-guard.middleware';
 
 export const driversRouter = Router({});
+
+// driversRouter.use(adminGuardMiddlewareAuth); // Применяем мидлвейр ко всем запросам в этом роутере
 
 // * Routes
 // ? method GET
@@ -25,14 +28,16 @@ driversRouter.get(
 // ? method POST
 driversRouter.post(
   '',
-  driverInputBodyDtoValidation,
-  inputValidationResultMiddleware,
-  createDriverHandler,
+  adminGuardMiddlewareAuth, // проверяет авторизацию для данного запроса.
+  driverInputBodyDtoValidation, // проверяет, что данные, передаваемые в теле запроса, соответствуют ожидаемой структуре.
+  inputValidationResultMiddleware, // проверяет, прошли ли данные валидацию.
+  createDriverHandler, // основной обработчик запроса, который создаёт водителя, если все предыдущие мидлвейры прошли успешно.
 );
 
 // ? method DELETE
 driversRouter.delete(
   '/:id',
+  adminGuardMiddlewareAuth,
   idParamValidation,
   inputValidationResultMiddleware,
   deleteDriverHandler,
@@ -41,6 +46,7 @@ driversRouter.delete(
 // ? method UPDATE
 driversRouter.put(
   '/:id',
+  adminGuardMiddlewareAuth,
   idParamValidation,
   driverInputBodyDtoValidation,
   inputValidationResultMiddleware,
@@ -48,6 +54,7 @@ driversRouter.put(
 );
 driversRouter.patch(
   '/:id',
+  adminGuardMiddlewareAuth,
   idParamValidation,
   inputValidationResultMiddleware,
   updateDriverPatchHandler,
