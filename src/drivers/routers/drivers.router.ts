3368b943569d5843ab1f4a7cb0eabf2d65ae8611
eduard.tobilko <1992eduard777@gmail.com>
2025-09-23 20:17:27 -0400

    lesson_2-added middleware-validations

--- src/drivers/routers/drivers.router.ts
+++ src/drivers/routers/drivers.router.ts
@@ -6,13 +6,20 @@ import { createDriverHandler } from './handlers/create-driver.handler';
 import { deleteDriverHandler } from './handlers/delete-driver.handler';
 import { updateDriverPutHandler } from './handlers/update-driver-put.handler';
 import { updateDriverPatchHandler } from './handlers/update-driver-patch.handler';
+import { idValidation } from '../../core/middlewares/validation/params-id-validation.middleware';
+import { inputValidationResultMiddleware } from '../../core/middlewares/validation/input-validation-result.middleware';
 
 export const driversRouter = Router({});
 
 // * Routes
 // ? method GET
 driversRouter.get('', getDriverListHandler);
-driversRouter.get(`/:driverId`, getDriverHandler);
+driversRouter.get(
+  `/:driverId`,
+  idValidation,
+  inputValidationResultMiddleware,
+  getDriverHandler,
+);
 
 // ? method POST
 driversRouter.post('', createDriverHandler);
